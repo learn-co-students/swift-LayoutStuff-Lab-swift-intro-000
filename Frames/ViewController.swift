2c5b32bd42eb9bbe3ea24c38d7adcecb26916153
othmansafarini <othman_safarini@live.co.uk>
2017-02-25 14:36:01 +0400

    Easy

--- Frames/ViewController.swift
+++ Frames/ViewController.swift
@@ -10,6 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
     var yellowSquare: UIView?
+    //frameXSlider, frameYSlider, boundsXSlider, and boundsYSlider
+    //frameXLabel, frameYLabel, boundsXLabel, and boundsYLabel
+    
+    
+    //image view
+    @IBOutlet weak var imageView: UIImageView!
+    
+    //IBOutlet for sliders
+    @IBOutlet weak var frameXSlider: UISlider!
+    @IBOutlet weak var frameYSlider: UISlider!
+    @IBOutlet weak var boundsXSlider: UISlider!
+    @IBOutlet weak var boundsYSlider: UISlider!
+    
+    //IBOutlet for labels
+    @IBOutlet weak var frameXLabel: UILabel!
+    @IBOutlet weak var frameYLabel: UILabel!
+    @IBOutlet weak var boundsXLabel: UILabel!
+    @IBOutlet weak var boundsYLabel: UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
